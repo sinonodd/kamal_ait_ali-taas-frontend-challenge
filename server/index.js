fe888a1de1989a88f75e361eb6b694be73ee7173
sinonodd <kamal8aly545@gmail.com>
2021-04-22 14:17:53 +0000

    :sparkles: generated a basic express server for oauth route and callback

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const volleyball = require('volleyball');
+const app = express();
+const auth = require('./auth')
+
+app.use(volleyball);
+app.get('/', (req,res) => {res.json({message:"Hello express" })});
+
+function notFound(req,res,next) {
+    res.status(404);
+    const error = new Error('not Found', req.originUrl);
+    next(error);
+}
+// auth route
+app.use('/auth', auth);
+
+function errorHandler(err,req,res,next){
+    res.status(res.statusCode || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+const port = process.env.PORT ||5000;
+app.listen(port,() => {console.log('listening on port ', port)
+});
+
+app.use(notFound);
+app.use(errorHandler);
